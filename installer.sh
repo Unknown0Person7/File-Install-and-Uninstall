2245ced79f2cb86b64d9e97eae64fa08811e4be7
Jonayed Hossen <jonayedanimator@gmail.com>
2024-09-28 17:31:04 +0600

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -6,19 +6,11 @@ cat << EOF
 ###########################################
 EOF
 echo ""
-read -p "Can You Use Linux [y or n only ]: " input
-if [ input = y ]
-then
-	sudo chmod+x main.sh
-	cat << EOF
-	Installed on Application Installer and Uninstaller
-	Run Application Installer and Uninstaller Using This Command
-	$ sudo ./main.sh
-	EOF
-else
-	cat << EOF
-	Problem For Installing 
-	Application Installer and Uninstaller Installing Fail
-	Try Agin
-	EOF
-fi
+sudo chmod +x main.sh
+cat << EOF
+Installed on Application Installer and Uninstaller
+
+Run Application Installer and Uninstaller Using This Command
+
+$ sudo ./main.sh
+EOF
